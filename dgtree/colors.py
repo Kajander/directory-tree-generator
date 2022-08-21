02cfb324540b8047e87941a13b4b95ae9a8c4baa
Joakim Kajan <joakim.kajan@icloud.com>
2022-08-21 16:55:31 +0300

    Added colors.py to color folders and files

---
+++ dgtree/colors.py
@@ -0,0 +1,16 @@
+
+
+class Colors:
+
+    END = '\x1b[0m'
+
+    FILE = '\x1b[38;2;152;251;152m'
+    FOLDER = '\x1b[38;2;193;225;193m'
+
+    PYTHON_YELLOW = '\x1b[38;2;255;232;115m'
+    PYTHON_BLUE = '\x1b[38;2;75;139;190m'
+    MARKDOWN = '\x1b[38;2;222;49;99m'
+
+
+
+
