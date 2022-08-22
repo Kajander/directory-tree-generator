7cb69b9ff8102bec97e6b979bf2bd5645ea8e0b3
Joakim Kajan <joakim.kajan@icloud.com>
2022-08-22 15:03:59 +0300

    Removed .groovy and added few missing colors

--- dgtree/colors.py
+++ dgtree/colors.py
@@ -3,6 +3,7 @@
 # replace the brackets with Red, Green, Blue values of the color of your choosing
 # '\x1b[38;2;{R};{G};{B}m'
 
+
 class Colors:
 
     END = '\x1b[0m'
@@ -13,50 +14,33 @@ class Colors:
 
     PYTHON_YELLOW = '\x1b[38;2;255;232;115m'        # https://www.schemecolor.com/python-logo-colors.php
     PYTHON_BLUE = '\x1b[38;2;75;139;190m'
-
     MARKDOWN = '\x1b[38;2;222;49;99m'
-
+    JSON = '\x1b[38;2;207;217;91m'                  # https://icons8.com/icon/114474/json
     HTML = '\x1b[38;2;240;101;41m'                  # https://www.schemecolor.com/html5.php
-
     CSS = '\x1b[38;2;60;153;220m'                   # https://www.schemecolor.com/css-family-blue.php
-
     C_SHARP = '\x1b[38;2;155;73;147m'               # https://logotyp.us/logo/c-sharp/
-
     CEE = '\x1b[38;2;77;182;172m'                   # https://icons8.com/icon/40670/c-programming/color
-
     C_PLUS_PLUS = '\x1b[38;2;40;53;147m'            # https://icons8.com/icon/set/c%2B%2B-programming/color
-
     SWIFT = '\x1b[38;2;255;109;0m'                  # https://icons8.com/icon/24465/swift
-
     R_LANG = '\x1b[38;2;39;109;194m'                # https://www.schemecolor.com/r-programming-language-logo-colors.php
-
     GO = '\x1b[38;2;41;190;176m'                    # https://www.schemecolor.com/go-logo-colors.php
-
     TYPESCRIPT = '\x1b[38;2;0;122;204m'             # https://colorswall.com/palette/314
-
     PHP = '\x1b[38;2;120;124;181m'                  # https://www.schemecolor.com/php.php
-
     JAVASCRIPT_YELLOW = '\x1b[38;2;240;219;79m'     # https://www.schemecolor.com/javascript-logo-colors.php
     JAVASCRIPT_CHARCOAL = '\x1b[38;2;50;51;48m'
-
     JAVA_ORANGE = '\x1b[38;2;248;152;32m'           # https://colorswall.com/palette/170
-    JAVA_BLUE = '\x1b[38;2;83;130;161m'
-
+    JAVA_BLUE = '\x1b[38;2;83;130;161m'             
     SCALA = '\x1b[38;2;222;52;35m'                  # https://www.schemecolor.com/scala-logo-colors.php
-
-
-    #PERL = '\x1b[38;2;222;52;35m' 
-
+    PERL = '\x1b[38;2;0;64;101m'                    # https://dev.to/thibaultduponchelle/the-perl-complex-topic-of-logos-3161
     HASKELL = '\x1b[38;2;143;78;139m'               # https://icons8.com/icon/Lvn3jvfnl1XF/haskell
-    
     DOTNET = '\x1b[38;2;81;43;212m'                 # https://github.com/dotnet/brand
-
-    MYSQL_SEABLUE = '\x1b[38;2;0;117;143M'          # https://www.schemecolor.com/mysql.php
+    MYSQL_SEABLUE = '\x1b[38;2;0;117;143m'          # https://www.schemecolor.com/mysql.php
     MYSQL_BEER = '\x1b[38;2;242;145;17m'
+    POSTGRESQL = '\x1b[38;2;0;139;185m'             # https://wiki.postgresql.org/wiki/Color_Palette
+    SQLITE = '\x1b[38;2;37;150;190m'                # https://commons.wikimedia.org/wiki/File:SQLite370.svg#/media/File:SQLite370.svg
+    LUA = '\x1b[38;2;0;0;128m'                      # https://logotyp.us/logo/lua/
+    RUST = '\x1b[38;2;247;76;0m'                    # https://www.pngitem.com/middle/ioRTmo_rust-programming-language-logo-hd-png-download/
+    RUBY = '\x1b[38;2;213;31;6m'                    # https://www.schemecolor.com/ruby-logo-colors.php
+    XML = '\x1b[38;2;0;0;255m'                      # https://upload.wikimedia.org/wikipedia/commons/9/9d/Xml_logo.svg
 
-    POSTGRESQL = '\x1b[38;2;0;139;185m'            # https://wiki.postgresql.org/wiki/Color_Palette
-
-    #SQLITE = '\x1b[38;2;0;139;185m'
-
-    #GROOVY = 
 
