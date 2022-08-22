0fb8394ca6aba337fdd6ee1f2baea2878f57b5d2
Joakim Kajan <joakim.kajan@icloud.com>
2022-08-22 14:17:43 +0300

    Added bunch of icons

--- dgtree/icons.py
+++ dgtree/icons.py
@@ -1,12 +1,35 @@
-
+# Nerd fonts 'nf-mdi'
 
 class Icons:
 
-    FOLDER_CLOSE = '  '
-    FOLDER_OPEN = ' ﱮ '
-    FILE = '  '
-    PYTHON = '  '
-    MARKDOWN = '  '
+    FOLDER_CLOSE = '  '    # nf-mdi
+    FOLDER_OPEN = ' ﱮ '     # nf-mdi
+    FILE = '  '            # nf-mdi
 
-
-
+    PYTHON = '  '          # nf-mdi
+    MARKDOWN = '  '        # nf-mdi
+    JSON =' ﬥ '             # nf-mdi
+    HTML = '  '            # nf-mdi
+    CSS = '  '             # nf-mdi
+    C_SHARP = '  '         # nf-mdi
+    CEE = ' ﭰ '             # nf-mdi
+    C_PLUS_PLUS = ' ﭱ '     # nf-mdi
+    SWIFT = ' ﯣ '           # nf-mdi
+    R_LANG = ' ﳒ '          # nf-mdi
+    GO = ' ﳑ '              # nf-mdi
+    TYPESCRIPT = ' ﯤ '      # nf-mdi
+    PHP = '  '             # nf-mdi
+    JAVASCRIPT = '  '      # nf-mdi
+    JAVA = '  '            # nf-dev
+    SCALA = '  '           # nf-dev
+    PERL = '  '            # nf-dev
+    HASKELL = '  '         # nf-dev
+    DOTNET = '  '          # nf-dev
+    MYSQL = '  '           # nf-dev
+    POSTGRESQL = '  '      # nf-dev
+    SQLITE = '  '          # nf-dev
+    GROOVY = '  '          # nf-dev
+    LUA = '  '             # nf-seti
+    RUST = '  '            # nf-dev
+    RUBY = '  '            # nf-dev
+    XML = '  '              # nf-mdi
